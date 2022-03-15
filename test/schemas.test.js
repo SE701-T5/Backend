d74e9f39de753eaeb308678203050b50a8b3dc23
kimslor <68843643+kimslor@users.noreply.github.com>
2022-03-16 11:29:41 +1300

    Removed forum post schema and test.

--- test/schemas.test.js
+++ test/schemas.test.js
@@ -1,23 +1,6 @@
 const expect = require('chai').expect;
  
-var forum = require('../config/db_schemas/forum.schema');
 var user = require('../config/db_schemas/user.schema');
- 
-describe('Forum post schema test', function() {
-    it('should be invalid if required fields are empty', function(done) {
-        var f = new forum();
- 
-        f.validate(function(err) {
-            expect(err.errors.userID).to.exist;
-            expect(err.errors.title).to.exist;
-            expect(err.errors.bodyText).to.exist;
-            expect(err.errors.editted).to.exist;
-            expect(err.errors.upVotes).to.exist;
-            expect(err.errors.downVotes).to.exist;
-            done();
-        });
-    });
-});
 
 describe('Forum user schema test', function() {
     it('should be invalid if required fields are empty', function(done) {
