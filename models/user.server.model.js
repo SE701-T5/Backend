dedcd3e41f7834026d90e65ab1dbd906e74f0c45
kimslor <68843643+kimslor@users.noreply.github.com>
2022-03-15 17:16:50 +1300

    Added comments.

--- models/user.server.model.js
+++ models/user.server.model.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema(
-{
+{   
+    // Username used for login
     username: {
         type: String,
         required: true,
@@ -11,23 +12,27 @@ const userSchema = new Schema(
         trim: true,
         minlength: 3
     },
+    // This is the user's public display name
     name: {
         type: String,
         required: true,
         trim: true,
         minlength: 3
     },
+    // Email associated to the account
     email: {
         type: String,
         required: true,
         trim: true
     },
+    // Hashed password used for login and verification
     hashedPassword: {
         type: String,
         required: true
     }
 });
 
+// User can be used to create new documents with the userSchema
 const User = mongoose.model('User', userSchema);
 
 module.exports = User;
