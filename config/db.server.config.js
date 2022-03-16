ee98790e566ad15b1f3082beefaa0f2e91f2e1a6
Adam Ross <14985050+R055A@users.noreply.github.com>
2022-03-16 18:03:56 +1300

    Implement database connection (#36)
    * Implement database connection
    
    * Fix package-lock.json

--- config/db.server.config.js
+++ config/db.server.config.js
@@ -1,1 +1,43 @@
 const database = require('mongoose');
+
+/**
+ * Configure and connect to MongoDB database
+ * Uses environment variables:
+ *  - DATABASE_USER
+ *  - DATABASE_PW
+ * @param databaseName the name of the database being connected to
+ */
+function connect(databaseName) {
+    // Database URI
+    const databaseURI = `mongodb://${process.env.DATABASE_USER}:${process.env.DATABASE_PW}` +
+        `@uniforumcluster-shard-00-00.wvdq3.mongodb.net:27017,uniforumcluster-shard-00-01.` +
+        `wvdq3.mongodb.net:27017,uniforumcluster-shard-00-02.wvdq3.mongodb.net:27017/` +
+        `${databaseName}?replicaSet=atlas-13bh8b-shard-0&authSource=admin&w=majority`;
+
+    // Database options
+    const databaseOptions = {
+        useNewUrlParser: true,
+        ssl: true,
+        retryWrites: true
+    };
+
+    return database.connect(databaseURI, databaseOptions);
+}
+
+/**
+ * Retures database connection state
+ * @returns {ConnectionStates} 0 for disconnected, 2 for connected, 1 for connecting, 3 for disconnecting
+ */
+function getState() {
+    return database.connection.readyState;
+}
+
+/**
+ * Closes database connection
+ * @returns {Promise<void>}
+ */
+function closeConn() {
+    return database.disconnect();
+}
+
+module.exports = { connect, getState, closeConn };
