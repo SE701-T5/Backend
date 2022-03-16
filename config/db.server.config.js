77fb97fa3ff1eb47ab5d53a6ff136a6e13b5e8e9
Adam Ross <14985050+R055A@users.noreply.github.com>
2022-03-17 00:50:21 +1300

    Implement insert new forum post to db add tests fix database schema fix schema test fix database configuration for testing

--- config/db.server.config.js
+++ config/db.server.config.js
@@ -1,4 +1,6 @@
-const database = require('mongoose');
+const
+    database = require('mongoose'),
+    testDatabase = require('mongoose');
 
 /**
  * Configure and connect to MongoDB database
@@ -6,8 +8,9 @@ const database = require('mongoose');
  *  - DATABASE_USER
  *  - DATABASE_PW
  * @param databaseName the name of the database being connected to
+ * @param isTestDatabase conditional for if the database in use is for testing
  */
-function connect(databaseName) {
+function connect(databaseName, isTestDatabase=false) {
     // Database URI
     const databaseURI = `mongodb://${process.env.DATABASE_USER}:${process.env.DATABASE_PW}` +
         `@uniforumcluster-shard-00-00.wvdq3.mongodb.net:27017,uniforumcluster-shard-00-01.` +
@@ -21,22 +24,33 @@ function connect(databaseName) {
         retryWrites: true
     };
 
-    return database.connect(databaseURI, databaseOptions);
+    // returns testing database connection if being used for testing, otherwise normal database connection
+    if (isTestDatabase) {
+        return testDatabase.connect(databaseURI, databaseOptions);
+    } else {
+        return database.connect(databaseURI, databaseOptions);
+    }
 }
 
 /**
- * Retures database connection state
+ * Returns database connection state
+ * @param isTestDatabase conditional for if the database in use is for testing
  * @returns {ConnectionStates} 0 for disconnected, 2 for connected, 1 for connecting, 3 for disconnecting
  */
-function getState() {
-    return database.connection.readyState;
+function getState(isTestDatabase=false) {
+    if (isTestDatabase) {
+        return testDatabase.connection.readyState;
+    } else {
+        return database.connection.readyState;
+    }
 }
 
 /**
  * Closes database connection
+ * @param isTestDatabase conditional for if the database in use is for testing
  * @returns {Promise<void>}
  */
-function closeConn() {
+function closeConn(isTestDatabase=false) {
     return database.disconnect();
 }
 
--- test/schemas.test.js
+++ test/schemas.test.js
@@ -1,7 +1,7 @@
-const 
-      expect = require('chai').expect,
-      user = require('../config/db_schemas/user.schema'),
-      forum = require('../config/db_schemas/forum.schema');
+const
+    expect = require('chai').expect,
+    user = require('../config/db_schemas/user.schema'),
+    forum = require('../config/db_schemas/forum.schema');
 
 describe('Forum user schema test', function() {
     it('should be invalid if required fields are empty', function(done) {
@@ -23,9 +23,10 @@ describe('Forum post schema test', function() {
  
         f.validate(function(err) {
             expect(err.errors.userID).to.exist;
+            expect(err.errors.communityID).to.exist;
             expect(err.errors.title).to.exist;
             expect(err.errors.bodyText).to.exist;
-            expect(err.errors.editted).to.exist;
+            expect(err.errors.edited).to.exist;
             expect(err.errors.upVotes).to.exist;
             expect(err.errors.downVotes).to.exist;
             done();
