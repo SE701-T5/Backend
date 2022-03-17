2bb52fe368c5a319663c6b5783a0778cf0aaa83e
Kimsong <68843643+kimslor@users.noreply.github.com>
2022-03-17 14:48:24 +1300

    Update models/user.server.model.js
    Co-authored-by: Adam Ross <14985050+R055A@users.noreply.github.com>

--- models/user.server.model.js
+++ models/user.server.model.js
@@ -5,16 +5,23 @@ const User = require('../config/db_schemas/user.schema')
  * @param {String} username User's username used for login
  * @param {String} name User's name used for public display
  * @param {String} email User's email associated to their account
- * @param {String} hashedPassword User's hased passsword used for login and verification
+ * @param {String} hashedPassword User's hashed password used for login and verification
+ * @param done function callback, returns status code, and message if error, or JSON if successful
  */
- exports.create = function(username, name, email, hashedPassword) {
+ exports.create = function(username, name, email, hashedPassword, done) {
     const newUser = new User({
         username,
         name,
         email,
         hashedPassword
-    })
+    });
     newUser.save()
-        .then(() => res.status(200).json('User added!'))
-        .catch(err => res.status(400).json('Error: ' + err));
+        .then((res) => done(res))
+        .catch((err) => {
+            if (err.code === 11000) {
+                return done({err: "Conflict", status: 409});
+            }
+            return done({status: 500, err: err})
+        });
 }
+
