59b996f82bd035706c0d14c3d0c9812f0b06931f
Kimsong <68843643+kimslor@users.noreply.github.com>
2022-03-17 20:50:21 +1300

    HTTP POST request for user creation (#37)
    * Added endpoint for forum user creation.
    
    * Moved http response code to controller.
    
    * Added unit test for forum user creation request
    
    * Password passed to hashPassword method (TBA).
    
    * Moved hashing of password to the controller.
    
    * Moved saving user info to db to model.
    
    * Added todo for delete request.
    
    * Added unit test for unsuccessful requests.
    
    * Added async function to ensure testDB is used.
    
    * Update models/user.server.model.js
    
    Co-authored-by: Adam Ross <14985050+R055A@users.noreply.github.com>
    
    * Update controllers/user.server.controller.js
    
    Co-authored-by: Adam Ross <14985050+R055A@users.noreply.github.com>
    
    * Update test/user.test.js
    
    Co-authored-by: Adam Ross <14985050+R055A@users.noreply.github.com>
    
    * Change reponse status return on test.
    
    * Indentation error.
    
    * Added required connected to db.server.config
    
    * Change expected status response.
    
    * Update test/user.test.js
    
    Co-authored-by: Adam Ross <14985050+R055A@users.noreply.github.com>
    
    * Update test/user.test.js
    
    Co-authored-by: Adam Ross <14985050+R055A@users.noreply.github.com>
    
    * Changed return status code.
    
    * Update test/user.test.js
    
    Co-authored-by: Adam Ross <14985050+R055A@users.noreply.github.com>
    
    * Update test/user.test.js
    
    Co-authored-by: Adam Ross <14985050+R055A@users.noreply.github.com>
    
    * Added bad request checks. Name is now displayName
    
    * Removed res send message, just return 201.
    
    * Return message with 201 response.
    
    * Updated name to displayName.
    
    * Updated name to displayName.
    
    * Updated condition for user params.
    
    * Fixed user params condition bug.
    
    Co-authored-by: Adam Ross <14985050+R055A@users.noreply.github.com>

--- controllers/user.server.controller.js
+++ controllers/user.server.controller.js
@@ -1,13 +1,61 @@
 const User = require('../models/user.server.model');
 
 /**
+ * Returns the hashPassword given a plaintextPassword
+ * @param {String} password
+ */
+ function hashPassword(password) {
+    // TODO: Create a has function for the password
+    return 'password'
+}
+
+/**
  * Creates a new forum user using HTTP request object data
  * @param req HTTP request object
  * @param res HTTP request response object
  */
 exports.userCreate = function(req, res) {
-    // TODO: implement userCreate()
-    res.json({ dummyTest: "userCreate() dummy test passes" });
+    const reqBody = req.body;
+    let
+        isBadRequest = false,
+        forumUserParams;
+
+    // Check that every expected forum user attribute exists in the request body
+    if (!reqBody.username || !reqBody.displayName || !reqBody.email || !reqBody.password) {
+        isBadRequest = true;
+    }
+
+    if (!isBadRequest) {
+        forumUserParams = {
+            'username': reqBody.username.length < 3 ? false : reqBody.username,
+            'displayName': reqBody.displayName.length < 3 ? false : reqBody.displayName,
+            'email': reqBody.email.length > 0 ? reqBody.email : false,
+            'hashedPassword': reqBody.password.length > 0 ? hashPassword(reqBody.password) : false
+        }
+    }
+
+    // Confirm that forum post attribute values are valid
+    for (let key in forumUserParams) {
+        if (forumUserParams[key] === false) {
+            isBadRequest = true;
+            break;
+        }
+    }
+
+    if (!isBadRequest) {
+        User.create(forumUserParams, function(result) {
+            if (result.err) {
+                // Return the error message with the error status
+                res.status(result.status).send(result.err);
+            } else {
+                // User was created successfully, return 201 status
+                res.status(201).send("User created successfully!");
+            }
+        });
+    }
+    else {
+        res.status(400).send("Bad request");
+    }
 }
 
 /**
