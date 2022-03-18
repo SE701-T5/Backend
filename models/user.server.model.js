4635365dda1a780c22064de8492ff8910a47f7b0
AlexMiao7 <63859123+AlexMiao7@users.noreply.github.com>
2022-03-18 15:14:02 +1300

    adding feature of user search (#35)
    * adding feature of user search
    
    * remove view by name/username function
    
    * add user search and unit test
    
    * fixed by suggestion
    
    * fixed unit test "View forum user by ID successfully"
    sending back json/user id after user is created
    
    Co-authored-by: Adam Ross <14985050+R055A@users.noreply.github.com>

--- models/user.server.model.js
+++ models/user.server.model.js
@@ -1,3 +1,4 @@
+
 const User = require('../config/db_schemas/user.schema')
 
 /**
@@ -30,4 +31,22 @@ const User = require('../config/db_schemas/user.schema')
             return done({err: "Internal server error", status: 500});
         });
 }
+
+/**
+ * Search user by id in the database
+ * @param id user id
+ * @param done function callback, returns status code, and message if error, or JSON if successful
+ */
+exports.searchById = function(id,done) {
+    try {
+        User.findById(id)
+            .then((res) => done(res))
+            .catch((err) => {
+                return done({status: 404, err: err})
+            });
+    } catch (err) {
+        done({ status: 500, err: err });
+    }
+}
+
 
