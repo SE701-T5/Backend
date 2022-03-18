e5dd9ce6013e6672771e47d1bb5d51c1a380aa84
Adam Ross <14985050+R055A@users.noreply.github.com>
2022-03-18 21:39:56 +1300

    Implement user update add test cases for user update refactor password hash (dummy) function to model add email validation refactor user controller to use lib functions, reduce redundancy

--- test/user.test.js
+++ test/user.test.js
@@ -1,14 +1,21 @@
 const
     { closeConn, connect } = require("../config/db.server.config"),
+    { resetCollections } = require("../models/db.server.model"),
     request = require('supertest'),
+    assert = require("assert"),
     app = require('../server');
 
 beforeEach(async function() {
     const testDatabaseName = process.env.DATABASE_TEST_NAME;
     await closeConn(); // Disconnect from the app database
     await connect(testDatabaseName, true); // Connect to the test database
+    await resetCollections(); // reset database for testing
 });
 
+after(async function() {
+    await resetCollections(); // reset database for testing
+    await closeConn(true); // Disconnect from the app database
+});
 
 describe("Create forum user successfully", function() {
     it("should return: status 201", function(done) {
@@ -114,11 +121,11 @@ describe("View forum user by ID successfully", function() {
     });
 });
 
-describe("View forum user by ID unsuccessfully", function() {
-    it('should return a 404 response for invalid id',function(done) {
+describe("View forum user by ID unsuccessfully with invalid ID", function() {
+    it('should return a 400 response for invalid id',function(done) {
         request(app)
             .get('/api/v1/users/x')
-            .expect(404)
+            .expect(400)
             .end(function(err, res) {
                 if (err) done(err);
                 done();
@@ -127,15 +134,271 @@ describe("View forum user by ID unsuccessfully", function() {
     });
 });
 
-describe("Update forum user by ID dummy test", function() {
-    it("should return: { dummyTest: 'userUpdateById() dummy test passes' }", function(done) {
+/**
+ * Test successful forum user database document update using valid ID
+ */
+describe("Update forum user successfully with valid ID", function() {
+    it("should return: 201", function(done) {
         request(app)
-            .patch('/api/v1/users/:id')
-            .send({ dummyTestInput: 'this text is useless' })
-            .expect({ dummyTest: 'userUpdateById() dummy test passes' })
+            .post('/api/v1/users')
+            .send(
+                {
+                    username: "NewBob",
+                    displayName: "Bobby",
+                    email: "bobby@joe.com",
+                    password: "BobbyJoe"
+                })
+            .expect(201)
             .end(function(err, res) {
                 if (err) done(err);
-                done();
+                request(app)
+                    .patch(`/api/v1/users/${res.body.user._id}`)
+                    .send(
+                        {
+                            username: "OldBob",
+                            displayName: "Bob",
+                            email: "joe@bobby.com",
+                            password: "JoeBob"
+                        })
+                    .expect(201)
+                    .end(function(err, res) {
+                        if (err) done(err);
+                        assert.equal(res.body.updatedForumUser.username, "OldBob");
+                        assert.equal(res.body.updatedForumUser.displayName, "Bob");
+                        assert.equal(res.body.updatedForumUser.email, "joe@bobby.com");
+                        assert.equal(res.body.updatedForumUser.hashedPassword, "JoeBob");
+                        done();
+                    });
+            });
+    });
+});
+
+/**
+ * Test unsuccessful forum user database document update using valid but un-matching ID
+ */
+describe("Update forum user unsuccessfully with valid but un-matching ID", function() {
+    it("should return: 404", function(done) {
+        request(app)
+            .post('/api/v1/users')
+            .send(
+                {
+                    username: "NewBob",
+                    displayName: "Bobby",
+                    email: "bobby@joe.com",
+                    password: "BobbyJoe"
+                })
+            .expect(201)
+            .end(function(err, res) {
+                if (err) done(err);
+                request(app)
+                    .patch(`/api/v1/users/62328e357ec0006e40e1b29b`)
+                    .send(
+                        {
+                            username: "WrongBob",
+                            displayName: "NotFound",
+                            email: "what@email.com",
+                            password: "password"
+                        })
+                    .expect(404)
+                    .end(function(err, res) {
+                        if (err) done(err);
+                        done();
+                    });
+            });
+    });
+});
+
+/**
+ * Test unsuccessful forum user database document update using invalid ID
+ */
+describe("Update forum user unsuccessfully with invalid ID", function() {
+    it("should return: 400", function(done) {
+        request(app)
+            .post('/api/v1/users')
+            .send(
+                {
+                    username: "NewBob",
+                    displayName: "Bobby",
+                    email: "bobby@joe.com",
+                    password: "BobbyJoe"
+                })
+            .expect(201)
+            .end(function(err, res) {
+                if (err) done(err);
+                request(app)
+                    .patch(`/api/v1/users/abc`)
+                    .send(
+                        {
+                            username: "NewBob",
+                            displayName: "Bobby",
+                            email: "bobby@joe.com",
+                            password: "BobbyJoe"
+                        })
+                    .expect(400)
+                    .end(function(err, res) {
+                        if (err) done(err);
+                        done();
+                    });
+            });
+    });
+});
+
+/**
+ * Test unsuccessful forum user database document update using empty update object
+ */
+describe("Update forum user unsuccessfully with empty update object", function() {
+    it("should return: 400", function(done) {
+        request(app)
+            .post('/api/v1/users')
+            .send(
+                {
+                    username: "NewBob",
+                    displayName: "Bobby",
+                    email: "bobby@joe.com",
+                    password: "BobbyJoe"
+                })
+            .expect(201)
+            .end(function(err, res) {
+                if (err) done(err);
+                request(app)
+                    .patch(`/api/v1/users/${res.body.user._id}`)
+                    .send(
+                        {
+
+                        })
+                    .expect(400)
+                    .end(function(err, res) {
+                        if (err) done(err);
+                        done();
+                    });
+            });
+    });
+});
+
+/**
+ * Test unsuccessful forum user database document update using invalid username update field
+ */
+describe("Update forum user unsuccessfully with invalid username update field", function() {
+    it("should return: 400", function(done) {
+        request(app)
+            .post('/api/v1/users')
+            .send(
+                {
+                    username: "NewBob",
+                    displayName: "Bobby",
+                    email: "bobby@joe.com",
+                    password: "BobbyJoe"
+                })
+            .expect(201)
+            .end(function(err, res) {
+                if (err) done(err);
+                request(app)
+                    .patch(`/api/v1/users/${res.body.user._id}`)
+                    .send(
+                        {
+                            "username": "yo"
+                        })
+                    .expect(400)
+                    .end(function(err, res) {
+                        if (err) done(err);
+                        done();
+                    });
+            });
+    });
+});
+
+/**
+ * Test unsuccessful forum user database document update using invalid displayName update field
+ */
+describe("Update forum user unsuccessfully with invalid displayName update field", function() {
+    it("should return: 400", function(done) {
+        request(app)
+            .post('/api/v1/users')
+            .send(
+                {
+                    username: "NewBob",
+                    displayName: "Bobby",
+                    email: "bobby@joe.com",
+                    password: "BobbyJoe"
+                })
+            .expect(201)
+            .end(function(err, res) {
+                if (err) done(err);
+                request(app)
+                    .patch(`/api/v1/users/${res.body.user._id}`)
+                    .send(
+                        {
+                            "displayName": "A"
+                        })
+                    .expect(400)
+                    .end(function(err, res) {
+                        if (err) done(err);
+                        done();
+                    });
+            });
+    });
+});
+
+/**
+ * Test unsuccessful forum user database document update using invalid email update field
+ */
+describe("Update forum user unsuccessfully with invalid email update field", function() {
+    it("should return: 400", function(done) {
+        request(app)
+            .post('/api/v1/users')
+            .send(
+                {
+                    username: "NewBob",
+                    displayName: "Bobby",
+                    email: "bobby@joe.com",
+                    password: "BobbyJoe"
+                })
+            .expect(201)
+            .end(function(err, res) {
+                if (err) done(err);
+                request(app)
+                    .patch(`/api/v1/users/${res.body.user._id}`)
+                    .send(
+                        {
+                            "email": "not an email"
+                        })
+                    .expect(400)
+                    .end(function(err, res) {
+                        if (err) done(err);
+                        done();
+                    });
+            });
+    });
+});
+
+/**
+ * Test unsuccessful forum user database document update using invalid password update field
+ */
+describe("Update forum user unsuccessfully with invalid password update field", function() {
+    it("should return: 400", function(done) {
+        request(app)
+            .post('/api/v1/users')
+            .send(
+                {
+                    username: "NewBob",
+                    displayName: "Bobby",
+                    email: "bobby@joe.com",
+                    password: "BobbyJoe"
+                })
+            .expect(201)
+            .end(function(err, res) {
+                if (err) done(err);
+                request(app)
+                    .patch(`/api/v1/users/${res.body.user._id}`)
+                    .send(
+                        {
+                            "password": ""
+                        })
+                    .expect(400)
+                    .end(function(err, res) {
+                        if (err) done(err);
+                        done();
+                    });
             });
     });
 });
