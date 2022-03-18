2c6817251b5e6f05e131017ee36230b4c3a24bda
jshi911 <wndyddld12@gmail.com>
2022-03-19 12:38:36 +1300

    Added the comment-related components.

--- models/db.server.model.js
+++ models/db.server.model.js
@@ -1,6 +1,7 @@
 const
     Forum = require("../config/db_schemas/forum.schema"),
-    User = require("../config/db_schemas/user.schema");
+    User = require("../config/db_schemas/user.schema"),
+    Comment = require("../config/db_schemas/comment.schema");
 
 /**
  * Remove all documents in the database collections
@@ -11,17 +12,23 @@ exports.resetCollections = async () => {
     try {
         await Forum.deleteMany();
     } catch (err) {
-        result = {status: 500, err: err};
+        result = { status: 500, err: err };
     }
 
     try {
         await User.deleteMany();
     } catch (err) {
-        result = {status: 500, err: err};
+        result = { status: 500, err: err };
+    }
+
+    try {
+        await Comment.deleteMany();
+    } catch (err) {
+        result = { status: 500, err: err };
     }
 
     if (!result) {
-        result = {status: 200};
+        result = { status: 200 };
     }
     return result;
 }
