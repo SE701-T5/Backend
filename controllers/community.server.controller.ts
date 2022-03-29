c244edb52e3a21634de95073eaab42de4f8faa37
John Jia <chenhao0401@hotmail.com>
2022-03-29 21:14:28 +1300

    feat:implement POST communities

--- controllers/community.server.controller.ts
+++ controllers/community.server.controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
-import * as Community from '../config/db_schemas/community.schema';
+import * as Community from '../models/community.server.model';
+import * as User from '../models/user.server.model';
 import config from '../config/config.server.config';
 import {
   isValidDocumentID,
@@ -7,3 +8,45 @@ import {
   isAnyFieldValid,
   isAllFieldsValid,
 } from '../lib/validate.lib';
+
+
+export function communityCreate(req: Request, res: Response) {
+  const authToken = req.get(config.get('authToken')),
+    reqBody = req.body;
+
+  // Set community post fields to an object for passing to the model
+  const communityParams = {
+    userID: isValidDocumentID(reqBody.userID) ? reqBody.userID : false,
+    name:
+      reqBody.name.length && reqBody.name.length > 0 ? reqBody.name : false,
+    description: reqBody.description || '',
+    members: reqBody.members || 0,
+    img: reqBody.img || '',
+  };
+
+  if (isAllFieldsValid(communityParams)) {
+    User.isUserAuthorized(communityParams.userID, authToken, function (result) {
+      if (result.isAuth) {
+        // Insert new community post to database
+        Community.insertCommunity(communityParams, function (result) {
+          if (result.err) {
+            // Return the error message with the error status
+            res.status(result.status).send(result.err);
+          } else {
+            // Return the community post document object with 201 status
+            res.status(201).json({ communityPostData: result });
+          }
+        });
+      } else {
+        if (result.err) {
+          // Return the error message with the error status
+          res.status(result.status).send(result.err);
+        } else {
+          res.status(401).send('Unauthorized');
+        }
+      }
+    });
+  } else {
+    res.status(400).send('Bad request');
+  }
+}
--- models/community.server.model.ts
+++ models/community.server.model.ts
@@ -1,1 +1,34 @@
 import Community from '../config/db_schemas/community.schema';
+
+
+
+export function insertCommunity(params, done) {
+    // Set forum Community attributes
+    const name = params.name,
+      description = params.description,
+      members = params.members,
+      img = params.img;
+  
+    // Create new forum Community document
+    const newCommunity = new Community({
+      name,
+      description,
+      members,
+      img,
+    });
+  
+    // Save new forum Community document to database collection
+    newCommunity
+      .save()
+      .then((res) => {
+        return done(res);
+      })
+      .catch((err) => {
+        // Forum Community is already in the database with unique attributes, return duplicate conflict error
+        if (err.code === 11000) {
+          return done({ err: 'Conflict', status: 409 });
+        }
+        // Any other database error, return internal server error
+        return done({ err: 'Internal server error', status: 500 });
+      });
+  }
