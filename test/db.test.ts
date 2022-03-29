b23c9e37c2ec0952ca8ff1cf3e25f4ffa4096da6
wqsz7xn <wqsz7xn@protonmail.com>
2022-03-29 19:01:54 +1300

    partial fix database tests

--- test/db.test.ts
+++ test/db.test.ts
@@ -1,35 +1,18 @@
 import request from 'supertest';
 import app from '../server';
-import { closeConn, connect } from '../config/db.server.config';
+import { closeConn, connect, getState } from '../config/db.server.config';
 import { StatusCodes } from 'http-status-codes';
 
-/**
- * Before all tests, the app database is disconnected
- */
-before(async function () {
-  await closeConn(); // Disconnect from the app database
-});
+describe.only('Database', () => {
+  beforeEach(async () => {
+    await connect();
+  });
 
-/**
- * Before each test, the test database is connected
- */
-beforeEach(async function () {
-  const testDatabaseName = process.env.DATABASE_TEST_NAME;
-  await connect(); // Connect to the test database
-});
+  after(async () => {
+    await closeConn();
+  });
 
-/**
- * After all tests, the test database is disconnected
- */
-after(async function () {
-  await closeConn(); // Disconnect from the app database
-});
-
-/**
- * Test successful deletion of all collection documents from test database
- */
-describe('Successfully reset database test - remove all documents', function () {
-  it("should return: 200 'OK'", function (done) {
+  it('Reset', function (done) {
     request(app)
       .post('/api/v1/reset')
       .expect(StatusCodes.OK)
@@ -38,36 +21,20 @@ describe('Successfully reset database test - remove all documents', function () 
         done();
       });
   });
-});
 
-/**
- * Test unsuccessful deletion of collection documents from disconnected test database
- */
-describe('Unsuccessfully reset database test - database is not connected', function () {
-  it('should return: 500', function (done) {
-    closeConn() // Disconnect from the test database
-      .then(() => {
-        request(app)
-          .post('/api/v1/reset')
-          .expect(StatusCodes.INTERNAL_SERVER_ERROR)
-          .end(function (err, res) {
-            if (err) done(err);
-            done();
-          });
-      });
+  it('Reset on closed connection', async () => {
+    closeConn().then(() => {
+      request(app)
+        .post('/api/v1/reset')
+        .expect(StatusCodes.INTERNAL_SERVER_ERROR);
+    });
   });
-});
 
-// XXX: Awaiting implementation
-describe('Resample database dummy test', function () {
-  it("should return: { dummyTest: 'resampleDB() dummy test passes' }", function (done) {
-    request(app)
-      .post('/api/v1/resample')
-      .send({ dummyTestInput: 'this text is useless' })
-      .expect({ dummyTest: 'resampleDB() dummy test passes' })
-      .end(function (err, res) {
-        if (err) done(err);
-        done();
-      });
-  });
+  // XXX: Dummy awaiting implementation
+  // it('Reset on closed connection (dummy)', async () => {
+  //   request(app)
+  //     .post('/api/v1/resample')
+  //     .send({ dummyTestInput: 'this text is useless' })
+  //     .expect({ dummyTest: 'resampleDB() dummy test passes' });
+  // });
 });
