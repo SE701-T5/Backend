f08fd5343a4d7abda0be4534624b0671e8361eea
simoncheng987 <simoncheng987@gmail.com>
2022-03-30 00:35:42 +1300

    Feat: Implement the Upadate Community Endpoint #100 - Added Update Community feature - Refactor the logic for auth & param retrieve

--- controllers/community.server.controller.ts
+++ controllers/community.server.controller.ts
@@ -7,3 +7,87 @@ import {
   isAnyFieldValid,
   isAllFieldsValid,
 } from '../lib/validate.lib';
+import { commentUpdateById } from './forum.server.controller';
+import { updateUserById } from '../models/community.server.model';
+
+export function userUpdateById(req: Request, res: Response) {
+  const authToken = req.get(config.get('authToken'));
+  const reqBody = req.body;
+  const communityID = req.params.id;
+
+  // Set community fields to an object for passing to the model
+  const communityParams = {
+    userID: isValidDocumentID(reqBody.userID) ? reqBody.userID : false,
+    name: reqBody.name.length && reqBody.name.length > 0 ? reqBody.name : false,
+    description: reqBody.description || '',
+    members: reqBody.members || 0,
+    img: reqBody.img || '',
+  };
+
+  if (!isAllFieldsValid(communityParams)) {
+    res.status(400).send('Bad request');
+  }
+
+  User.isUserAuthorized(
+    communityParams.userID,
+    authToken,
+    function (authResult) {
+      if (authResult.err) {
+        res.status(authResult.status).send(authResult.err);
+      } else if (!authResult.isAuth) {
+        res.status(401).send('Unauthorized');
+      }
+
+      Community.updateUserById(
+        communityID,
+        communityParams,
+        function (updateResult) {
+          if (updateResult.err) {
+            res.status(updateResult.status).send(updateResult.err);
+          }
+          res.status(201).json({ communityPostData: updateResult });
+        },
+      );
+    },
+  );
+}
+
+export function communityCreate(req: Request, res: Response) {
+  const authToken = req.get(config.get('authToken')),
+    reqBody = req.body;
+
+  // Set community fields to an object for passing to the model
+  const communityParams = {
+    userID: isValidDocumentID(reqBody.userID) ? reqBody.userID : false,
+    name: reqBody.name.length && reqBody.name.length > 0 ? reqBody.name : false,
+    description: reqBody.description || '',
+    members: reqBody.members || 0,
+    img: reqBody.img || '',
+  };
+
+  if (isAllFieldsValid(communityParams)) {
+    User.isUserAuthorized(communityParams.userID, authToken, function (result) {
+      if (result.isAuth) {
+        // Insert new community to database
+        Community.insertCommunity(communityParams, function (result) {
+          if (result.err) {
+            // Return the error message with the error status
+            res.status(result.status).send(result.err);
+          } else {
+            // Return the community document object with 201 status
+            res.status(201).json({ communityPostData: result });
+          }
+        });
+      } else {
+        if (result.err) {
+          // Return the error message with the error status
+          res.status(result.status).send(result.err);
+        } else {
+          res.status(401).send('Unauthorized');
+        }
+      }
+    });
+  } else {
+    res.status(400).send('Bad request');
+  }
+}
--- models/community.server.model.ts
+++ models/community.server.model.ts
@@ -1,1 +1,66 @@
 import Community from '../config/db_schemas/community.schema';
+
+export function insertCommunity(params, done) {
+  // Set Community attributes
+  const name = params.name,
+    description = params.description,
+    members = params.members,
+    img = params.img;
+
+  // Create new Community document
+  const newCommunity = new Community({
+    name,
+    description,
+    members,
+    img,
+  });
+
+  // Save new Community document to database collection
+  newCommunity
+    .save()
+    .then((res) => {
+      return done(res);
+    })
+    .catch((err) => {
+      // Community is already in the database with unique attributes, return duplicate conflict error
+      if (err.code === 11000) {
+        return done({ err: 'Conflict', status: 409 });
+      }
+      // Any other database error, return internal server error
+      return done({ err: 'Internal server error', status: 500 });
+    });
+}
+
+export function updateUserById(CommunityID, params, done) {
+  // Set Community attributes
+  const name = params.name,
+    description = params.description,
+    members = params.members,
+    img = params.img;
+
+  // Create new Community document
+  const newCommunity = new Community({
+    name,
+    description,
+    members,
+    img,
+  });
+
+  try {
+    Community.findOneAndUpdate(
+      { _id: CommunityID },
+      { $set: params },
+      { new: true },
+    )
+      .then((res) => {
+        return res
+          ? done({ status: 200, res })
+          : done({ err: 'Not found', status: 404 });
+      })
+      .catch((err) => {
+        return done({ err: 'Not found', status: 404 });
+      });
+  } catch (err) {
+    return done({ err: 'Internal server error', status: 500 });
+  }
+}
